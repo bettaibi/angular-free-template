7c37f021c99f71dc8eb00bb4dcff6d62f4a92f2d
bettaibi <bettaibinidhal00@gmail.com>
2021-03-17 11:58:44 +0100

    Angular Material Schematics components have been added

--- src/app/layouts/admin/admin.module.ts
+++ src/app/layouts/admin/admin.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 const routes: Routes = [
-  { path: '', children: [
+  { path: '', component: AdminComponent , children: [
     {path: '', pathMatch: 'full', redirectTo: '/statistics'},
     { path: 'statistics', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }
   ]}
@@ -14,7 +20,13 @@ const routes: Routes = [
   declarations: [AdminComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ]
 })
 export class AdminModule {}
--- src/app/layouts/admin/home/home.module.ts
+++ src/app/layouts/admin/home/home.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { Routes, RouterModule } from '@angular/router';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
 
 const routes: Routes = [
   {path: '', component: HomeComponent}
@@ -13,7 +19,13 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule
   ]
 })
 export class HomeModule { }
