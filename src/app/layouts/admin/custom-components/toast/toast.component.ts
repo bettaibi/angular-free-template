2f743f928e5c7c477ec0a1b82b2e6054d3d3121a
bettaibi <bettaibinidhal00@gmail.com>
2021-03-28 14:54:37 +0200

    find out observable operators

--- src/app/layouts/admin/custom-components/toast/toast.component.ts
+++ src/app/layouts/admin/custom-components/toast/toast.component.ts
@@ -1,34 +1,43 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { AnimationBuilder, AnimationPlayer, animate, style } from '@angular/animations';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription, of, pipe } from 'rxjs';
+import { filter, map, catchError, retry } from 'rxjs/operators'
+import { ajax, AjaxResponse } from 'rxjs/ajax'
 
 @Component({
   selector: 'app-toast',
   templateUrl: './toast.component.html',
   styleUrls: ['./toast.component.scss']
 })
-export class ToastComponent implements OnInit {
-  player: AnimationPlayer;
-  @ViewChild('para') element: ElementRef;
+export class ToastComponent implements OnInit, OnDestroy {
+  myObservable: Observable<any>;
+  myUnsubscription: Subscription;
+  url = "https://jsonplaceholder.typicode.com/todos";
 
-  constructor(
-    private animationBuilder: AnimationBuilder
-  ) {}
+  constructor() {}
 
   ngOnInit(): void {
+      this.myObservable = ajax(this.url).pipe(
+        map((res: AjaxResponse)=>{
+          if(!res.response){
+            throw new Error('An error has been occured')
+          }
+          return res.response;
+        }),
+        retry(1),
+        catchError(err => of([])),
+      )
   }
 
-  changeColor(): void{
-    this.player = this.animationBuilder
-    .build([
-      // style({backgroundColor: 'lightblue'}),
-      animate('1s ease', style({backgroundColor: 'green'}))
-    ])
-    .create(this.element.nativeElement);
+  increment(): void{
+    this.myUnsubscription = this.myObservable.subscribe((data)=>
+    {console.log(data)},
+    (err) => console.info(err.message),
+    () => console.log('all tasks are completed')
+    )
+  }
 
-    setTimeout(()=>{
-      this.player.play();
-   
-    }, 0)
+  ngOnDestroy(): void{
+    this.myUnsubscription.unsubscribe()
   }
 
 }
