71460fc62677da0436fab16e56d227dfbfbb5517
bettaibi <bettaibinidhal00@gmail.com>
2021-05-02 19:20:07 +0200

    reusable angular animations

--- src/app/layouts/admin/custom-components/toast/toast.component.ts
+++ src/app/layouts/admin/custom-components/toast/toast.component.ts
@@ -1,23 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, animate, style, transition } from '@angular/animations';
+import { customAnimations } from '@BN/animations';
 
 @Component({
   selector: 'app-toast',
   templateUrl: './toast.component.html',
   styleUrls: ['./toast.component.scss'],
-  animations: [
-    trigger('fadeColor', [
-
-      transition(':enter', [
-        style({ backgroundColor: 'red'}),
-        animate('1s ease-out', style({ backgroundColor: 'green'}))
-      ]),
-      transition(':leave', [
-        style({ backgroundColor: 'green'}),
-        animate('1s ease-out', style({ backgroundColor: 'red'}))
-      ]),
-    ])
-  ]
+  animations: customAnimations
 })
 export class ToastComponent implements OnInit {
 
